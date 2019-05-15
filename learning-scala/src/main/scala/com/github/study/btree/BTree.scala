39beb4f3b5c8b9fc43ddb44bb72844049aa3dfaa
jang <dragoalie@gmail.com>
2019-05-16 00:26:17 +0900

    Second commit, WIP

--- learning-scala/src/main/scala/com/github/study/btree/BTree.scala
+++ learning-scala/src/main/scala/com/github/study/btree/BTree.scala
@@ -1,25 +1,27 @@
 package com.github.study.btree
 
-/**
- * Created by shintaro.tamaki on 2015/10/29.
- */
+
 sealed trait Node {
   def size: Int
 }
 
 case class Leaf(value: Int) extends Node {
-  def size: Int = ???
+  def size: Int = 1
 }
 
-case class Branch(left:  Node, value: Int, right: Node) extends Node {
-  def size: Int = ???
+case class Branch(left: Node, value: Int, right: Node) extends Node {
+  def size: Int = left.size + 1 + right.size
 }
 
 case class BTree(node: Node) {
-  def size: Int = ???
+  def size: Int = node.size
+
   def max: Int = ???
+
   def min: Int = ???
+
   def avg: Int = ???
+
   def sum: Int = ???
 }
 
