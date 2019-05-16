67723862b4708ed7b3b2cfc98b88775af551462f
jang <dragoalie@gmail.com>
2019-05-17 01:02:20 +0900

    Refactor test

--- learning-scala/src/test/scala/com/github/study/wordcount/WordCountSpec.scala
+++ learning-scala/src/test/scala/com/github/study/wordcount/WordCountSpec.scala
@@ -13,8 +13,7 @@ class WordCountSpec extends FunSpec with Matchers {
       val ans_ = target.countFruitsFromLines_(lines)
 
       ans should be(Map("banana" -> 1, "muscat" -> 1, "orange" -> 3, "mango" -> 2, "apple" -> 3, "kiwi" -> 1, "papaya" -> 1))
-
-      assert(ans_ === ans)
+      ans_ should be (ans)
 
     }
 
