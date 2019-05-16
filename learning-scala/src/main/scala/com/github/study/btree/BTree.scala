77a4aa03bb7970014824a9e949e8b6b9f9c8a4f5
jang <dragoalie@gmail.com>
2019-05-17 01:02:33 +0900

    BTree wip

--- learning-scala/src/main/scala/com/github/study/btree/BTree.scala
+++ learning-scala/src/main/scala/com/github/study/btree/BTree.scala
@@ -1,6 +1,5 @@
 package com.github.study.btree
 
-
 sealed trait Node {
   def size: Int
 }
@@ -14,18 +13,34 @@ case class Branch(left: Node, value: Int, right: Node) extends Node {
 }
 
 case class BTree(node: Node) {
+
   def size: Int = node.size
 
-  def max: Int = ???
+  def max: Int = node match { // most right
+    case Leaf(v) => v
+    case Branch(l, v, r) => Math.max(Math.max(BTree(l).max, v), BTree(r).max)
+  }
 
-  def min: Int = ???
+  def min: Int = node match { // most left
+    case l: Leaf => l.value
+    case b: Branch => Math.min(Math.min(BTree(b.left).min, b.value), BTree(b.right).min)
+  }
 
-  def avg: Int = ???
+  def sum: Int = node match {
+    case l@Leaf(_) => l.value
+    case b@Branch(_, _, _) => BTree(b.left).sum + b.value + BTree(b.right).sum
+  }
 
-  def sum: Int = ???
+  def avg: Int = sum / size // middle value
+
 }
 
 object BTree {
-  def apply(list: List[Int]): BTree = ???
+  def apply(list: List[Int]): BTree = list match {
+    case List(x) => BTree(Leaf(x))
+    case l@List(_, _, _) => BTree(Branch(Leaf(l.min), l.sum / 3, Leaf(l.max)))
+    case _ => ???
+  }
+
 }
 
