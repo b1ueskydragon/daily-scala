e8dd01891bb14979450bf1f444db9dcc5010c422
jang <dragoalie@gmail.com>
2019-05-18 11:22:33 +0900

    O(N) twice

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -9,4 +9,18 @@ object FizzBuzz {
     case _ => x.toString
   }
 
+  def fizzBuzz_(sortedInc: Range): Iterable[String] =
+    sortedInc.map {
+      x => (x % 3, x % 5, x)
+    }.map {
+      case (0, 0, _) => "FizzBuzz"
+      case (0, _, _) => "Fizz"
+      case (_, 0, _) => "Buzz"
+      case (_, _, x) => x.toString
+    }
+
+  def main(args: Array[String]): Unit = {
+    println(fizzBuzz_(1 to 20))
+  }
+
 }
