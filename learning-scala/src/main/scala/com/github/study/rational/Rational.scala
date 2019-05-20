f957615763325d604992acf7d32ca709277d4e27
jang <dragoalie@gmail.com>
2019-05-20 10:52:24 +0900

    Rational add - wip

--- learning-scala/src/main/scala/com/github/study/rational/Rational.scala
+++ learning-scala/src/main/scala/com/github/study/rational/Rational.scala
@@ -2,10 +2,24 @@ package com.github.study.rational
 
 // 分数
 case class Rational(n: Int, d: Int) {
-  def +(that: Rational): Rational = ???
+
+  private def lcm(x: Int, y: Int): Int = (x * y) / gcd(x, y)
+
+  private def gcd(x: Int, y: Int): Int = if (y == 0) Math.abs(x) else gcd(y, x % y)
+
+  def +(that: Rational): Rational = (d, that.d) match {
+    case (x, y) if x == y => Rational(n + that.n, d)
+    case (x, y) =>
+      val lcd = lcm(x, y)
+      Rational(d * (lcd / that.n) + that.d * (lcd / n), lcd)
+  }
 
   def <(that: Rational): Boolean = ???
+
   def >(that: Rational): Boolean = ???
+
   def <=(that: Rational): Boolean = ???
+
   def >=(that: Rational): Boolean = ???
+
 }
--- learning-scala/src/test/scala/com/github/study/rational/RationalSpec.scala
+++ learning-scala/src/test/scala/com/github/study/rational/RationalSpec.scala
@@ -5,15 +5,25 @@ import org.scalatest._
 class RationalSpec extends FunSpec with Matchers {
 
   describe("Rational") {
+
     describe("+") {
+
       it("should returns new rational") {
         val x = Rational(1, 3)
         val y = Rational(1, 3)
-        x + y should be (Rational(2, 3))
+        x + y should be(Rational(2, 3))
       }
+
+      it("should returns new rational even if have different denominators") {
+        val x = Rational(1, 15)
+        val y = Rational(1, 12)
+        x + y should be(Rational(9, 60)) // ?!
+      }
+
     }
-    
+
     describe("compare") {
+
       it("should compare rational") {
         val x = Rational(1, 3)
         val y = Rational(1, 2)
@@ -23,6 +33,9 @@ class RationalSpec extends FunSpec with Matchers {
         println(x <= y)
         println(x >= y)
       }
+
     }
+
   }
+
 }
