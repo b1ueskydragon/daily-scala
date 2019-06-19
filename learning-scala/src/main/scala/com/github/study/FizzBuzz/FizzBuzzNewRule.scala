2227f992e662a9e70a0b7eda485609874ce0be51
jang <dragoalie@gmail.com>
2019-06-19 11:42:09 +0900

    refactor

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzzNewRule.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzzNewRule.scala
@@ -1,54 +1,55 @@
 package com.github.study.FizzBuzz
 
-/** FizzBuzzルール変更
-  * - その２
-  *  - １行で書く
-  * - その３
-  *  - 2の倍数は除外する
-  * - その４
-  *  - １行ずつ表示をやめる
-  *  - カンマ区切りで１行で出す
-  * - その５
-  *  - その1 バージョンのFizzBuzzの出力前のリストから、
-  * Fizz/Buzz/FizzBuzz になっていない数値の足し算をして合計値を出す
-  * - その６
-  *  - 1 to 100のリストを作らない
-  */
 object FizzBuzzNewRule {
-
-  def fizzBuzz02(n: Int): String =
-    if (n % 15 == 0) "FizzBuzz" else if (n % 3 == 0) "Fizz" else if (n % 5 == 0) "Buzz" else s"$n"
-
 
   def fizzBuzz03(n: Int): String =
     if (n % 15 == 0) "FizzBuzz" else if (n % 3 == 0) "Fizz" else if (n % 5 == 0) "Buzz" else if (n % 2 == 0) "" else s"$n"
 
-  def fizzBuzz05(xs: List[Int]): Int = xs.filterNot(_ % 15 == 0).filterNot(_ % 5 == 0).filterNot(_ % 3 == 0).sum
+  def fizzBuzz05(xs: List[Int]): Int = xs.filterNot(x => x % 5 == 0 || x % 3 == 0).sum
+
+  def fizzBuzz06(from: Int): Stream[String] = {
+
+    def three: Stream[Option[String]] = Stream(None, None, Some("Fizz")) #::: three
+
+    def five: Stream[Option[String]] = Stream(None, None, None, None, Some("Buzz")) #::: five
+
+    three.zip(five).zip(Stream.from(1)).map {
+      case ((None, None), k) => k.toString
+      case ((f, b), _) => s"${f.getOrElse("")}${b.getOrElse("")}"
+    }
+
+  }
 
   def main(args: Array[String]): Unit = {
 
     val range = 1 to 20
 
-    println("Rule 02")
-    range.foreach { n => println(fizzBuzz02(n)) }
+    rule("02") {
+      range.foreach { i => println(if (i % 15 == 0) "FizzBuzz" else if (i % 5 == 0) "Buzz" else if (i % 3 == 0) "Fizz" else i) }
+    }
 
+    rule("03") {
+      range.foreach { n => val fb = fizzBuzz03(n); if (fb != "") println(s"$fb") }
+    }
+
+    rule("04") {
+      range.foreach { n => val fb = fizzBuzz03(n); if (n == range.last) println(s"$fb") else if (fb != "") print(s"$fb,") }
+    }
+
+    rule("05") {
+      println(fizzBuzz05(range.toList))
+    }
+
+    rule("06") {
+      fizzBuzz06(range.head).take(range.last).foreach(println)
+    }
+
+  }
+
+  private def rule(num: String)(f: => Unit) {
+    println(s"[ RULE $num ]")
+    f
     println()
-
-    println("Rule 03")
-    range.foreach { n => val fb = fizzBuzz03(n); if (fb != "") println(s"$fb") }
-
-    println()
-
-
-    println("Rule 04")
-    range.foreach { n => val fb = fizzBuzz03(n); if (n == range.last) print(s"$fb") else if (fb != "") print(s"$fb,") }
-
-    println()
-    println()
-
-    println("Rule 05")
-    println(fizzBuzz05(range.toList))
-
   }
 
 }
